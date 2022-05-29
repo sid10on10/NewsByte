a693b349fd24151e1839c93b8e69d42df2b30168
Siddhant Singh <siddhantk.singh.phe14@itbhu.ac.in>
2022-05-29 22:14:43 +0530

    updated mongodb url

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 var mongodb = require('mongodb')
 var mongodClient = mongodb.MongoClient;
 var url = "mongodb+srv://siddhant:#rebirth19@cluster0.bkof1.mongodb.net/?retryWrites=true&w=majority";
-//var url = "mongodb://localhost:27017";
 
 module.exports = {url,mongodClient}
