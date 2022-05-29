6a4d65b9cab50589e45466e54333c20612c00407
Siddhant Singh <siddhantk.singh.phe14@itbhu.ac.in>
2022-05-29 22:06:47 +0530

    updated mongo db password

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 var mongodb = require('mongodb')
 var mongodClient = mongodb.MongoClient;
-var url = "mongodb+srv://siddhant:qwerty123@cluster0.bkof1.mongodb.net/?retryWrites=true&w=majority";
+var url = "mongodb+srv://siddhant:#rebirth19@cluster0.bkof1.mongodb.net/?retryWrites=true&w=majority";
+//var url = "mongodb://localhost:27017";
 
 module.exports = {url,mongodClient}
